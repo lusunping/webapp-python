9392964f3180ab7fd698ad7870d5b8e4acd5a133
Lusunping <l.sunping@gami.com>
2016-07-28 11:28:16 +0800

    add app.py -- async web application

---
+++ www/app.py
@@ -0,0 +1,31 @@
+#!C:\Users\Evol\Desktop\python\webapp-python
+#-*- coding: utf-8 -*-
+
+__author__  = 'Lu Sunping'
+
+'''
+async web application
+'''
+
+#由于Web app是建立在asyncio的基础上的，所以用aiohttp写一个基本的app.py
+
+import logging; logging.basicConfig(level=logging.INFO)
+import asyncio, os, json, time
+from datetime import datetime
+from aiohttp import web
+
+def index(request):
+	return web.Response(body=b'<h1>Awesome</h1>')
+	   
+@asyncio.coroutine
+def init(loop):
+	app = web.Application(loop = loop)
+	app.router.add_route('GET','/',index)
+	srv = yield from loop.create_server(app.make_handler(),'127.0.0.1',9000)
+	logging.info("Server started at http://127.0.0.1...")
+	return srv
+	
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
+	
