f05638bc74ccb7e76de8a15b824ab27c079b9ab2
Lusunping <l.sunping@gami.com>
2016-08-01 14:53:35 +0800

    change app.py

--- www/app.py
+++ www/app.py
@@ -16,11 +16,15 @@ from aiohttp import web
 
 def index(request):
 	return web.Response(body=b'<h1>Awesome</h1>')
+	
+def index_home(request):
+	return web.Response(body=b'<h1>Home</h1>')
 	   
 @asyncio.coroutine
 def init(loop):
 	app = web.Application(loop = loop)
 	app.router.add_route('GET','/',index)
+	app.router.add_route('GET','/home',index_home)
 	srv = yield from loop.create_server(app.make_handler(),'127.0.0.1',9000)
 	logging.info("Server started at http://127.0.0.1...")
 	return srv
